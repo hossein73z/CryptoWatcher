8be9e81666436e157000e7897b3c6341fc53cade
Hossein Zalaghi <hossein73.z@gmail.com>
2023-03-23 12:21:53 +0430

    change price type from integer to flaot

--- priceWatcher/models.py
+++ priceWatcher/models.py
@@ -5,7 +5,7 @@ class Pair(models.Model):
     id = models.IntegerField(primary_key=True)
     currency = models.TextField(null=False)
     base = models.TextField(null=False)
-    price = models.IntegerField(null=False, default=0)
+    price = models.FloatField(null=False, default=0)
     price_date = models.DateTimeField(null=False)
 
     class Meta:
