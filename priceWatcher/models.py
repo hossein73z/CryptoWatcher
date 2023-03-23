9b677fc9d0cb35a5c9de53d59e09a36baf311f05
Hossein Zalaghi <hossein73.z@gmail.com>
2023-03-23 11:17:16 +0430

    Creted pair model

--- priceWatcher/models.py
+++ priceWatcher/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Pair(models.Model):
+    id = models.IntegerField(primary_key=True)
+    currency = models.TextField(null=False)
+    base = models.TextField(null=False)
+    price = models.IntegerField(null=False, default=0)
+    price_date = models.DateTimeField(null=False)
+
+    class Meta:
+        db_table = 'pairs'
