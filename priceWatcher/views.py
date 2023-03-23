b3b25af4a528c5cfbb47c129e9e5d80122028d68
Hossein Zalaghi <hossein73.z@gmail.com>
2023-03-23 11:57:28 +0430

    Add datato be sent to pair_list template

--- priceWatcher/views.py
+++ priceWatcher/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from priceWatcher.models import Pair
+
 
 # Create your views here.
 def pair_list(request):
-    return render(request, 'pair_list.html', {})
+    pairs = Pair.objects.all().values()
+    return render(request, 'pair_list.html', {'pairs': pairs})
