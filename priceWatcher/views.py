2ce963d40622a2966c16e443fe5b58ecc8ccfedd
Hossein Zalaghi <hossein73.z@gmail.com>
2023-03-23 16:30:08 +0430

    Bug fix: template not showing price date

--- priceWatcher/views.py
+++ priceWatcher/views.py
@@ -5,5 +5,5 @@ from priceWatcher.models import Pair
 
 # Create your views here.
 def pair_list(request):
-    pairs = Pair.objects.all().values()
+    pairs: list[Pair] = Pair.objects.all().values()
     return render(request, 'pair_list.html', {'pairs': pairs})
