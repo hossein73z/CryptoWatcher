7af6077fc19d7fafc36aac094b9471947900468c
Hossein Zalaghi <hossein73.z@gmail.com>
2023-03-23 11:35:04 +0430

    Created admin interface for 'pairs' table

--- priceWatcher/admin.py
+++ priceWatcher/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from priceWatcher.models import Pair
+
+
+class PairAdmin(admin.ModelAdmin):
+    list_display = ('id', 'currency', 'base', 'price', 'price_date')
+
+
 # Register your models here.
+admin.site.register(Pair, PairAdmin)
